ce8c3f505b56e4d3b7874fea303def152607948e
Franco Traversaro <f.traversaro@gmail.com>
2016-10-11 02:20:46 +0200

    prende forma!

--- DungeonEcology/Core.py
+++ DungeonEcology/Core.py
@@ -28,32 +28,55 @@ class Herbivore(Animal):
 
 class Room:
     def __init__(self, height: int, width: int):
-        self.height = height
-        self.width = width
+        self.height = height  # type: int
+        self.width = width  # type: int
         self.saprofites = []  # type: List[Saprofite]
         self.vegetables = []  # type: List[Vegetable]
         self.carnivores = []  # type: List[Carnivore]
         self.herbivores = []  # type: List[Herbivore]
 
-    def add_saprofite(self, race: str):
-        self.saprofites.append(Saprofite(race))
+    @staticmethod
+    def __add_creature(list_var: List, class_name, race):
+        creature = class_name(race)
+        list_var.append(creature)
+        return creature
 
-    def add_vegetable(self, race: str):
-        self.vegetables.append(Vegetable(race))
+    def add_saprofite(self, race: str) -> Saprofite:
+        return self.__add_creature(self.saprofites, Saprofite, race)
 
-    def add_carnivore(self, race: str):
-        self.carnivores.append(Carnivore(race))
+    def add_vegetable(self, race: str) -> Vegetable:
+        return self.__add_creature(self.vegetables, Vegetable, race)
 
-    def add_herbivore(self, race: str):
-        self.herbivores.append(Herbivore(race))
+    def add_carnivore(self, race: str) -> Carnivore:
+        return self.__add_creature(self.carnivores, Carnivore, race)
+
+    def add_herbivore(self, race: str) -> Herbivore:
+        return self.__add_creature(self.herbivores, Herbivore, race)
+
+    @staticmethod
+    def __print(title, list_var: List[LifeForm]):
+        print(' ', title)
+        for creature in list_var:
+            print('  *', creature.race)
+
+    def print(self):
+        self.__print('vegetali', self.vegetables)
+        self.__print('erbivori', self.herbivores)
+        self.__print('carnivori', self.carnivores)
+        self.__print('saprofiti', self.saprofites)
 
 
 class Dungeon:
     def __init__(self, name):
-        self.name = name
-        self.rooms = []
+        self.name = name  # type: str
+        self.rooms = []  # type: List[Room]
 
     def create_room(self, height: int, width: int) -> Room:
         room = Room(height, width)
         self.rooms.append(room)
         return room
+
+    def print(self):
+        for level, room in enumerate(self.rooms):
+            print('Livello', level)
+            room.print()
--- run.py
+++ run.py
@@ -6,11 +6,5 @@ r = dungeon.create_room(23, 30)
 r.add_carnivore('wolf')
 r.add_herbivore('sheep')
 
-for level, room in enumerate(dungeon.rooms):
-    print('Livello', level)
-    print('erbivori')
-    for creature in room.herbivores:
-        print('*', creature.race)
-    print('carnivori')
-    for creature in room.carnivores:
-        print('*', creature.race)
+
+dungeon.print()
