d0361f09e057c176fe3120d32fcb58635c776b12
Franco Traversaro <f.traversaro@gmail.com>
2016-10-16 18:00:36 +0200

    first interaction

--- run.py
+++ run.py
@@ -1,16 +1,5 @@
-# from DungeonEcology import Core
-#
-# dungeon = Core.Dungeon("Deep Dungeon")
-#
-# r = dungeon.create_room(23, 30)
-# r.add_carnivore('wolf')
-# r.add_herbivore('sheep')
-#
-#
-# dungeon.print()
-
 import kivy
-
+from DungeonEcology import Core
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -21,22 +10,16 @@ kivy.require('1.9.1')
 class MainScreen(BoxLayout):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.col_sx = BoxLayout(orientation='vertical')
-        self.add_widget(self.col_sx)
-        self.col_cent = BoxLayout(orientation='vertical')
-        self.add_widget(self.col_cent)
-        self.col_dx = BoxLayout(orientation='vertical')
-        self.add_widget(self.col_dx)
+        self.dungeon = Core.Dungeon('Dongione')
 
-        self.col_sx.add_widget(Label(text='Livelli'))
-        self.col_cent.add_widget(Label(text='Creature'))
-        self.col_dx.add_widget(Label(text='Azioni'))
+    def create_room(self):
+        self.dungeon.create_room(30, 40)
+        self.ids.levels.add_widget(Label(text='Un piano'))
 
 
 class DungeonEcologyApp(App):
     def build(self):
         return MainScreen()
-
 
 if __name__ == '__main__':
     DungeonEcologyApp().run()
