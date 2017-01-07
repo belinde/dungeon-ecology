b179df4f6f82d303e7ebcdf020f8bd6b0c7819d7
Franco Traversaro <f.traversaro@gmail.com>
2017-01-07 12:09:36 +0100

    added data.js

---
+++ scripts/data.js
@@ -0,0 +1,22 @@
+var DungeonEcologyDatabase = {
+    setup: {
+        startingMoney: 20000
+    },
+    roomtypes: {
+        cave: {
+            fertility: 50,
+            luminance: 5,
+            humidity: 70
+        },
+        stone: {
+            fertility: 10,
+            luminance: 50,
+            humidity: 20
+        },
+        wood: {
+            fertility: 20,
+            luminance: 50,
+            humidity: 15
+        }
+    }
+}
